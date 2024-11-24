2dbec323cb9678854d14d5f765595e909974af35
Mark Hogan <hoganmark2021@gmail.com>
2024-11-24 19:40:59 +0000

    Added new scene and fixed drone physics

--- DroneGamePrototype/Assets/Scripts/PlayerMovement.cs
+++ DroneGamePrototype/Assets/Scripts/PlayerMovement.cs
@@ -14,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
     public float movementSpeed = 10f;
     public float maxRotationSpeed = 100f;
     public float maxMovementSpeed = 10f;
-
+    public float speedMultiplier = 5f;
     public float Speed = 10f;
     // Start is called before the first frame update
     float leftHorizontal = 0;
@@ -46,7 +46,6 @@ public class PlayerMovement : MonoBehaviour
     }
     void Update()
     {
-        //Rotation Right and Left Stick
         Vector3 rotationVelocity = new Vector3(-rightHorizontal * rotationSpeed * rightMagnitude,
         leftHorizontal * rotationSpeed * leftMagnitude, -rightVertical * rotationSpeed * rightMagnitude);
         rig.angularVelocity = transform.TransformDirection(rotationVelocity);
@@ -56,14 +55,8 @@ public class PlayerMovement : MonoBehaviour
         if (leftVertical > 0)
         {
             Vector3 worldVelocity = transform.TransformDirection(newVelocity);
-            rig.velocity = worldVelocity;
+            worldVelocity *= speedMultiplier;
+            rig.AddForce(worldVelocity, ForceMode.VelocityChange);
         }
-        // Vector3 origin = transform.position + Vector3.up * 0.5f;
-
-        // Vector3 direction = Vector3.up;
-        // Vector3 rotatedDirection = rotationOffset * direction;
-        // Debug.DrawRay(origin, rotatedDirection * rayDistance, Color.red);
-
-
     }
 }
